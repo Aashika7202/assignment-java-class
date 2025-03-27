1ecdd2694b5b80d50e98e624daaf34a921a42b99
aashikabhattarai6@gmail.com <aashikabhattarai6@gmail.com>
2025-03-27 13:37:06 -0500

    added

--- src/introductionjavaclass02.java
+++ src/introductionjavaclass02.java
@@ -5,6 +5,7 @@ public class introductionjavaclass02 {
             System.out.println("Hello, I am an student of UTA");
             System.out.println("I am learning java");
             System.out.println("How can I help you today?");
+            System.out.println("i love doing yoga");
         }
     }
 
