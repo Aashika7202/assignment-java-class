43178e58eb34841fa433e26495ac55323198e8b1
aashikabhattarai6@gmail.com <aashikabhattarai6@gmail.com>
2025-03-27 14:33:50 -0500

    added

--- src/sumofintegers.java
+++ src/sumofintegers.java
@@ -1,6 +1,18 @@
 public class sumofintegers {
     public static void main(String[] args) {
-        System.out.println("hello");
 
-    }
+                // Declare two integers
+                int firstNumber = 5;
+                int secondNumber = 7;
+
+                // Add the two integers
+                int sum = firstNumber + secondNumber;
+
+                // Display the result
+                System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);{
+            }
+        }
+
+
+
 }
